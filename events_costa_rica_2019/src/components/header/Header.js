b456bf73bc8acf3ecc5e340241bfb944b2cf9109
frannet82 <frannet82@gmail.com>
2019-01-12 16:38:07 -0600

    Adding header

---
+++ events_costa_rica_2019/src/components/header/Header.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './Header.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import iconButton from '@material-ui/core/IconButton';
+import SideDrawer from './SideDrawer';
+
+class Header extends Component {
+    state ={
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll',this.handlesScroll);
+    }
+
+    handlesScroll = () =>{
+     window.scrollY > 0  ? this.setState({headerShow: true}) : this.setState({headerShow: false});
+    }
+
+    toggleDrawer = (value) => {
+        this.setState({ drawerOpen: value});
+    }
+
+    render() {
+        return (
+            <AppBar style={{backgroundColor: this.state.headerShow ?  'rgb(124, 124, 212)' : 'transparent'}} className='customBar'>
+                    <Toolbar>
+                        <div className="header_logo">
+                            <div className='font_header header_logo_venue'>Events 2019</div>
+                            <div className='header_logo_title'>Costa Rica</div>
+                        </div>
+                        <iconButton 
+                        arial-label="Menu" 
+                        color="inherint"
+                        onClick= {() => this.toggleDrawer(true)}
+                        >
+                            <MenuIcon/>
+                        </iconButton>
+                    </Toolbar>
+
+                    <SideDrawer 
+                    open={this.state.drawerOpen}
+                    onClose={(value) => this.toggleDrawer(value)}>
+                    </SideDrawer>
+
+            </AppBar>
+        );
+    }
+}
+
+export default Header;
---
+++ events_costa_rica_2019/src/components/header/SideDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './SideDrawer.css';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMusic } from '@fortawesome/free-solid-svg-icons/faMusic';
+import { faGuitar } from '@fortawesome/free-solid-svg-icons/faGuitar'
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+const SideDrawer = (props) => {
+    return (
+        <Drawer
+        anchor="right" 
+        open={props.open} 
+        onClose={() => props.onClose(false)}>
+
+            <List component="nav" className="customDrawer">
+                {['Events start in', 'Moments BAC', 'HighLights', 'Princing','Location'].map((text, index) => (
+                    <ListItem  className="item" button onClick={()=> console.log(text)}>
+                        <ListItemIcon  >{index % 2 === 0 ? <FontAwesomeIcon className="colorIcon1" icon={faMusic} /> : <FontAwesomeIcon className="colorIcon2" icon={faGuitar} />}</ListItemIcon>
+                        {text}
+                    </ListItem>
+                ))}
+             </List>
+             
+        </Drawer>
+    );
+};
+
+export default SideDrawer;
