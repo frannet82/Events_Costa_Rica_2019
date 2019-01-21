add8e84f917092f194d637e1a5722e316a2a35ca
frannet82 <frannet82@gmail.com>
2019-01-20 18:54:52 -0600

    Adding component BAC

---
+++ events_costa_rica_2019/src/components/bac/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Zoom from 'react-reveal/Zoom';
+import Icon_calendar from '../../resources/images/icons/calendar.png';
+import Icon_location from '../../resources/images/icons/location.png';
+
+const bac = () => {
+    const uuidv4 = require('uuid/v4');
+    return (
+        <div className="bck_black">
+            <div className="center_wrapper">
+                <div className="vn_wrapper">
+                <Zoom bottom timeout={1500}>
+                {[[Icon_calendar,'Event Date & Time','6 April 2019 19:00Hrs', 'vn_icon_square bck_red'],[Icon_location,'Event Location','National Stadium Costa Rica', 'vn_icon_square bck_yellow']].map((card, index) => (
+                    <div className="vn_item" key={uuidv4(card[1])}>
+                        <div className="vn_outer">
+                            <div className="vn_inner">
+                                <div className={card[3]} />
+                                <div className="vn_icon"
+                                         style={{
+                                             background: `url(${card[0]})`
+                                         }}
+                                    >
+                                </div>
+                                <div className="vn_title">
+                                     {card[1]}
+                                </div>
+                                <div className="vn_desc">
+                                     {card[2]}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                </Zoom>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default bac;
