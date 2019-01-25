12562408aae2b3e1308ee630aa4413d50bc7c460
frannet82 <frannet82@gmail.com>
2019-01-24 22:14:54 -0600

    Updates

---
+++ precache-manifest.6679db750b46e5db9a043ad6367bfa37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b57e66fa13543c341289923ab0d4b31",
+    "url": "/Events_Costa_Rica_2019/static/media/slide_two.5b57e66f.jpg"
+  },
+  {
+    "revision": "ade82189da964b996b254c0e38ce7dc6",
+    "url": "/Events_Costa_Rica_2019/static/media/slide_three.ade82189.jpg"
+  },
+  {
+    "revision": "517deea24a6e0efcd8ab5c0cf1b4583c",
+    "url": "/Events_Costa_Rica_2019/static/media/slide_one.517deea2.jpg"
+  },
+  {
+    "revision": "3dc7c2438393b9440cf0",
+    "url": "/Events_Costa_Rica_2019/static/js/runtime~main.3dc7c243.js"
+  },
+  {
+    "revision": "25079ed4200adbcfc905",
+    "url": "/Events_Costa_Rica_2019/static/js/main.25079ed4.chunk.js"
+  },
+  {
+    "revision": "c5699a6107924599b0c9",
+    "url": "/Events_Costa_Rica_2019/static/js/1.c5699a61.chunk.js"
+  },
+  {
+    "revision": "25079ed4200adbcfc905",
+    "url": "/Events_Costa_Rica_2019/static/css/main.8d9a3527.chunk.css"
+  },
+  {
+    "revision": "54a0ec0ff69079f62451b02e043c8633",
+    "url": "/Events_Costa_Rica_2019/index.html"
+  }
+];
